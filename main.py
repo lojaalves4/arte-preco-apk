cfc53d7bc6b608d1f954f05e5680dcf3bd5dd0e1
lojaalves4 <lojaalves4@gmail.com>
2026-02-28 12:07:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from kivy.app import App
 from kivy.uix.label import Label
 
+
 class ArtePreco(App):
     def build(self):
         return Label(text="Arte Preço Pro Offline - OK")
+
 
 if __name__ == "__main__":
     ArtePreco().run()
